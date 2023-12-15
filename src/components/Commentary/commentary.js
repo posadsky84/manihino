70c12afbe6b278fa68aaa9840516f998049ea4c1
posadsky84 <a.i.posadsky@gmail.com>
2023-12-15 20:48:07 +0300

    Начало верстки окна комментариев

--- src/components/Commentary/commentary.js
+++ src/components/Commentary/commentary.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ModalScreen from '../../helpers/modalScreen';
+import "./commentary.css";
 
 const Commentary = ({closeCallback}) => {
   return (
      <ModalScreen closeCallback={closeCallback}>
+       <div className="comm">
+         <div className="comm-head-area">
 
+         </div>
+         <div className="comm-chat-area">
+
+         </div>
+         <div className="comm-post-area">
+
+         </div>
+       </div>
     </ModalScreen>
   );
 };
